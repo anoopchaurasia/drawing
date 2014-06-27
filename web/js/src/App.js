0427d3e48ed14725c308e97f8aa92fd9f7e2213f
Anoop <anoop26667@gmail.com>
2014-06-27 09:23:32 +0530

    added layer actions in file menu

--- web/js/src/App.js
+++ web/js/src/App.js
@@ -2,11 +2,18 @@ angular.module('app', [])
 .directive('addImage',[function(){
 	return {
 		link: function (scope, element) {
+			var input = element.find("input");
 			element.change(function(e, data){
 			    var url = window.URL || window.webkitURL
 			    , src = url.createObjectURL(e.target.files[0]);
 			    scope.drawing.layerManager.addImageLayer(src, e.target.files[0].name);
 			    scope.$apply();
+			});
+			element.click(function (e) {
+				if(e.taget.nodeName === "INPUT"){
+					return false;
+				}
+				input.click();
 			});
 		}
 	}
